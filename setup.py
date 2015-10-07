cdd16af7fe17fc0b68418552b3e8a939e6f93d76
Per Classon <perwclasson@gmail.com>
2015-10-07 23:43:14 +0200

    Added setup.cfg

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup_kwargs = dict(
     license='BSD',
     author='Per Classon',
     author_email='perwclasson@gmail.com',
-    keywords=['dead code', 'unreachable code'],
+    url='https://github.com/perclasson/python-tombstones',
+    download_url='https://github.com/perclasson/python-tombstones/tarball/0.1',
     packages=['tombstones'],
     package_dir={'tombstones': 'tombstones'},
     entry_points={
@@ -15,6 +16,8 @@ setup_kwargs = dict(
     },
     install_requires=['wrapt>=1.10.5'],
     include_package_data=True,
+    keywords=['dead code', 'unreachable code'],
+    classifiers=[],
 )
 
 setup(**setup_kwargs)
