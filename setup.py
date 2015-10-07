6c17d7e291c10710cda1578a4d08d428ba999296
Per Classon <perwclasson@gmail.com>
2015-10-07 13:00:19 +0200

    Updated setup.py and improved log file reading.

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup
 setup_kwargs = dict(
     name='tombstones',
     version='0.1',
+    description='Python module to identify unreachable code.',
     license='BSD',
+    author='Per Classon',
+    author_email='perwclasson@gmail.com',
+    keywords=['dead code', 'unreachable code'],
     packages=['tombstones'],
     package_dir={'tombstones': 'tombstones'},
     entry_points={
